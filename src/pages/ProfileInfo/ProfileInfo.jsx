ab206663b622fc5f813c651efc43c476bc144a03
David Galeano <davidgaleano217@gmail.com>
2021-06-26 17:54:17 -0500

    Profile Info connected with api

--- src/pages/ProfileInfo/ProfileInfo.jsx
+++ src/pages/ProfileInfo/ProfileInfo.jsx
@@ -1,43 +1,43 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router';
 import { MainContainer } from './ProfileInfoStyles'
 import ExpertProfileInfo from '../../components/ExpertProfileInfo/ExpertProfileInfo'
 import ExpertProfileAgenda from '../../components/ExpertProfileInfo/ExpertProfileAgenda'
 
-import JsonData from "../../assets/json/MOCK_DATA_SINGLE.json"
-
 const ProfileInfo = () => {
 
-    // const { id } = useParams()
+    const { id } = useParams()
 
-     const [profile, setProfile] = useState(JsonData);
+    console.log(id)
 
+     const [profile, setProfile] = useState([]);
 
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         const data = await fetch(`https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/${id}`)
-    //         const profile = await data.json()
-    //         setProfile(profile)
-    //     }
-
-    //     getData();
-    // }, [id])
-
-    return (
-        <MainContainer>
+     useEffect(() => {
+         const getData = async () => {
+             const data = await fetch(`https://consultify.herokuapp.com/api/user/${id}/detail`)
+             const profileResponse = await data.json()
+             setProfile(profileResponse)
+            }
+            
+            getData();
+        }, [id])
+        
+        return (
+            <MainContainer>
             <ExpertProfileInfo 
-            	name={profile[0].name}
-            	profession={profile[0].profession}
-            	location={profile[0].location}
-            	technologies={profile[0].technologies}
-            	github={profile[0].github}
-            	linkedin={profile[0].linkedin}
-            	resume={profile[0].resume}
-            	description={profile[0].description}
-            	experience={profile[0].experience}
-            	education={profile[0].education}
-                avatar={profile[0].avatar}
+            	name={profile.name}
+            	profession={profile.profession}
+            	location={profile.location}
+            	technologies={profile.technologies}
+            	github={profile.github}
+            	linkedin={profile.linkedin}
+            	resume={profile.resume}
+            	description={profile.description}
+            	experience={profile.experience}
+            	education={profile.education}
+                avatar={profile.avatar}
             />
-            <ExpertProfileAgenda fee={profile[0].fee} />
+            <ExpertProfileAgenda fee={profile.fee} />
         </MainContainer>
     )
 }
