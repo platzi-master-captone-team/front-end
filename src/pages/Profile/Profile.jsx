343fdd879fe21ae8ba2b3ac54be9e94103f23cab
Denisse Rivas <iqrivas@gmail.com>
2021-06-28 22:03:59 -0500

    [UPDATE] Responsive User Profile Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import SectionMenu from '../../components/SectionMenu/SectionMenu';
 import ProfileSection from '../../components/ProfileSection/ProfileSection';
 import { LoginContext } from '../../utils/loginStatus';
+import { useHistory, useLocation } from 'react-router-dom';
 import {
     ProfileMain,
     Avatar,
@@ -9,13 +10,32 @@ import {
     UserInfo,
     UserName,
     UserRole,
-    Sidebar
+    Sidebar,
+    MenuSelect,
+    MenuOption
 } from './Profile.styles';
 
+
 const Profile = () => {
+    const history = useHistory();
+    const location = useLocation();
+
 
     const { login } = useContext(LoginContext);
     const avatarURL = 'https://ui-avatars.com/api/?name='+login.name+'&rounded=true&background=FFB900&bold=true';
+
+    const [menuSelect, setMenuSelect] = useState();
+
+    function changeSection (ev) {
+        
+        const toURL = "/profile/"+ ev.target.value;
+        history.push(toURL);
+    }
+
+    useEffect(() => {
+        let currentPath = location.pathname.split('/')[2];
+        setMenuSelect(currentPath)
+    }, [location]);
 
     return(
         <ProfileMain>
@@ -28,6 +48,16 @@ const Profile = () => {
                     </UserInfo>
                 </User>
                 <SectionMenu />
+                <MenuSelect id="menu-select" value={menuSelect} onChange={changeSection}>    
+                    <MenuOption value="dashboard">Dashboard</MenuOption>
+                    <MenuOption value="history">Historial de Citas</MenuOption>
+                    <MenuOption value="availability">Configuración de Disponibilidad</MenuOption>
+                    <MenuOption value="payment">Configuración de Módulo de Retiro</MenuOption>
+                    <MenuOption value="personal">Configuración Personal</MenuOption>
+                    <MenuOption value="profesional">Configuración Profesional</MenuOption>
+                    <MenuOption value="terms">Terminos y Condiciones</MenuOption>
+                </MenuSelect>
+                <hr />    <hr />
             </Sidebar>
             <ProfileSection/>
         </ProfileMain>
