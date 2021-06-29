56217e35ed140a62716fdc2e1faa9c7212f004d0
Denisse Rivas <iqrivas@gmail.com>
2021-06-29 18:36:11 -0500

    [ADD] Availability Section from Profile

--- src/components/Availability Responsive/Availability.jsx
+++ src/components/Availability Responsive/Availability.jsx
@@ -1,46 +1,41 @@
 import {  } from "../ProfileSection/ProfileSection.styles";
-import { WeekCalendar, Slot, Day, Label, LabelResp, TimeLabel, FormButton } from "./Availability.styles";
+import { WeekCalendar, Slot, Day, Label } from "./Availability.styles";
 
 const SectionAvailability = () => {
 
-    function handleClick (ev) {
-        console.log('Slot Clicked: ' + ev.target.dataset.avail)
-        
-    }
 
     return(
         <>
             <WeekCalendar>
                 <Day>
-                    <TimeLabel></TimeLabel>
-                    <TimeLabel>00:00</TimeLabel>
-                    <TimeLabel>01:00</TimeLabel>
-                    <TimeLabel>02:00</TimeLabel>
-                    <TimeLabel>03:00</TimeLabel>
-                    <TimeLabel>04:00</TimeLabel>
-                    <TimeLabel>05:00</TimeLabel>
-                    <TimeLabel>06:00</TimeLabel>
-                    <TimeLabel>07:00</TimeLabel>
-                    <TimeLabel>08:00</TimeLabel>
-                    <TimeLabel>09:00</TimeLabel>
-                    <TimeLabel>10:00</TimeLabel>
-                    <TimeLabel>11:00</TimeLabel>
-                    <TimeLabel>12:00</TimeLabel>
-                    <TimeLabel>13:00</TimeLabel>
-                    <TimeLabel>14:00</TimeLabel>
-                    <TimeLabel>15:00</TimeLabel>
-                    <TimeLabel>16:00</TimeLabel>
-                    <TimeLabel>17:00</TimeLabel>
-                    <TimeLabel>18:00</TimeLabel>
-                    <TimeLabel>19:00</TimeLabel>
-                    <TimeLabel>20:00</TimeLabel>
-                    <TimeLabel>21:00</TimeLabel>
-                    <TimeLabel>22:00</TimeLabel>
-                    <TimeLabel>23:00</TimeLabel>
+                    <Label></Label>
+                    <Label>00:00</Label>
+                    <Label>01:00</Label>
+                    <Label>02:00</Label>
+                    <Label>03:00</Label>
+                    <Label>04:00</Label>
+                    <Label>05:00</Label>
+                    <Label>06:00</Label>
+                    <Label>07:00</Label>
+                    <Label>08:00</Label>
+                    <Label>09:00</Label>
+                    <Label>10:00</Label>
+                    <Label>11:00</Label>
+                    <Label>12:00</Label>
+                    <Label>13:00</Label>
+                    <Label>14:00</Label>
+                    <Label>15:00</Label>
+                    <Label>16:00</Label>
+                    <Label>17:00</Label>
+                    <Label>18:00</Label>
+                    <Label>19:00</Label>
+                    <Label>20:00</Label>
+                    <Label>21:00</Label>
+                    <Label>22:00</Label>
+                    <Label>23:00</Label>
                 </Day>
                 <Day>
                     <Label>Lunes</Label>
-                    <LabelResp>Lun</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -68,7 +63,6 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Martes</Label>
-                    <LabelResp>Mar</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -96,7 +90,6 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Miércoles</Label>
-                    <LabelResp>Mié</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -124,7 +117,6 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Jueves</Label>
-                    <LabelResp>Jue</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -152,7 +144,6 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Viernes</Label>
-                    <LabelResp>Vie</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -180,7 +171,6 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Sábado</Label>
-                    <LabelResp>Sáb</LabelResp>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -208,8 +198,7 @@ const SectionAvailability = () => {
                 </Day>
                 <Day>
                     <Label>Domingo</Label>
-                    <LabelResp>Dom</LabelResp>
-                    <Slot id='dom0' active='' onClick={handleClick}></Slot>
+                    <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
                     <Slot></Slot>
@@ -235,7 +224,7 @@ const SectionAvailability = () => {
                     <Slot></Slot>
                 </Day>
             </WeekCalendar> 
-            <FormButton>Actualizar Disponibilidad</FormButton>
+            
         </>
     )
 }
