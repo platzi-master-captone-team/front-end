ffe9404dac8e2ab006790c0211411a9214f4fa77
Denisse Rivas <iqrivas@gmail.com>
2021-07-01 09:02:13 -0500

    [UPDATE] Banner link changed and message will change according to User role

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -15,13 +15,15 @@ const Banner = () => {
     }
 
     useEffect(() => {
-        setBanner({show: login.status, type:'chat', message:'Tienes un cita con tu cliente.'});
+        let bannerMsg = login.role === 'Experto' ? 'Tienes un cita con tu cliente' : 'Tienes un cita con tu consultor';
+        setBanner({show: login.status, type:'chat', message: bannerMsg});
       }, [login]);
 
     return(
         <BannerContainer $show={banner.show} >
             <BannerMessage>{banner.message} 
-                <BannerLink id="chat" to="/chat/holi" type={banner.type}>Únete Ahora</BannerLink>
+                <BannerLink id="chat" to="/chat/A123B4?name=Ana%20Rojas&avatar=women/51&date=Jueves%2015%20Julio%202021&time=17:00%20-%2018:00&status=active" 
+                type={banner.type}>Únete Ahora</BannerLink>
                 <BannerLink id="profile" to="/profile/dashboard" type={banner.type}>Ir al Perfil</BannerLink>
             </BannerMessage>
             <CloseBanner onClick={closeBanner}>X</CloseBanner>
